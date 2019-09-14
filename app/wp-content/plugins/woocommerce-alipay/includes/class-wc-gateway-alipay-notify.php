6cfaa30de85cdbdea479a7011532cc572e493877
rockts <rockts@sina.com>
2019-09-15 06:47:00 +0800

    整理代码

---
+++ app/wp-content/plugins/woocommerce-alipay/includes/class-wc-gateway-alipay-notify.php
@@ -0,0 +1,104 @@
+<?php 
+
+if( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
+class WC_Gateway_Alipay_Notify {
+  public function __construct(){
+    // 同步通知挂载(支付成功感谢页面)
+    add_action( 'woocommerce_thankyou_alipay', array( $this, 'return_handler' ) );
+    // 异步通知挂载
+    add_action( 'rest_api_init', array( $this, 'rest_api_init' ) );
+  }
+
+  public function return_handler( $order_id ) {
+    $vars = $_GET;
+    unset( $vars['key'] );
+
+    $order = wc_get_order( $order_id );
+    $gateway = wc_get_payment_gateway_by_order( $order );
+
+    $sign_verified = $this->verify_sign( $vars, $gateway );
+
+    WC_Gateway_Alipay::log( $vars, 'info', true );
+
+    // 更新订单状态
+    if ( $sign_verified && ( $order->get_status() === 'pending' ) ) {
+      $order->update_status( 'processing', '支付宝交易号：' . $vars['trade_no'] );
+      // 更新订单自定义字段
+      update_post_meta( $order_id, 'trade_no', $vars['trade_no'] );
+    }
+  }
+
+  // 验证支付宝数据签名
+  public function verify_sign( $data, $gateway ) {
+    $gateway->log( '--- 验证支付宝返回的数据 ---' );
+
+    $alipay_public_key = $gateway->alipay_public_key;
+    $sign_type = 'RSA2';
+
+    $gateway->aop_client->alipayrsaPublicKey = $alipay_public_key;
+    $sign_verified = $gateway->aop_client->rsaCheckV1( $data, $alipay_public_key, $sign_type );
+
+    if ( $sign_verified ) {
+      $gateway->log( '数据验证成功' );
+    } else {
+      $gateway->log( '数据验证失败', 'error' );
+    }
+
+    return $sign_verified;
+  }
+
+  // 处理异步通知
+  // 添加异步通知接口地址
+  public function rest_api_init() {
+    register_rest_route( 'alipay/v1', '/notify', array(
+      'methods'   => 'POST',
+      'callback'  => array( $this, 'notify_handler' ),
+    ) );
+  }
+
+  // 设置支付宝异步通知地址
+  public function notify_handler( $request ) {
+    global $woocommerce;
+    $body = $request->get_body_params();
+
+    $out_trade_no = str_replace( '(sandbox) - ', '', $body['out_trade_no'] );
+    $total_amount = $body['total_amount'];
+    $trade_status = $body['trade_status'];
+    $trade_no = $body['trade_no'];
+
+    $order = wc_get_order( $out_trade_no );
+
+    if ( ! $order ) {
+      return 'failure';
+    }
+
+    $gateway = wc_get_payment_gateway_by_order( $order );
+
+    if ( ( $order->get_tatal() !== $total_amount ) && ! $gateway->sandbox ) {
+      return 'failure';
+    }
+
+    $gateway->log( '--- 接受到支付宝异步通知 ---' );
+    // 验证签名
+    $sign_verified = $this->verify_sign( $body, $gateway );
+
+    if ( $sign_verified ) {
+
+      if ( ( $trade_status === 'TRADE_SUCCESS' ) && ( $order->get_status() === 'pending' ) ) {
+        $order->update_status( 'processing', '支付宝交易号：' . $trade_no );
+        $order->reduce_order_stock();
+        update_post_meta( $order->get_id(), 'trade_no', $trade_no );
+
+        // 清空购物车
+        $woocommerce->cart->empty_cart();
+      }
+
+      return 'success';
+    } else {
+      return 'failure';
+    }
+  }
+}
